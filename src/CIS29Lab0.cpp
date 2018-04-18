cc3a8a98aa59734d4e17ad3786a8abed2351ec67
Brad <32529373+bradosia@users.noreply.github.com>
2018-04-17 21:20:19 -0700

    edits

--- src/CIS29Lab0.cpp
+++ src/CIS29Lab0.cpp
@@ -6,8 +6,170 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include <iostream>
+#include <string>
+#include <fstream>
+#include <iostream>     // std::cout
+#include <algorithm>    // std::find
+#include <vector>       // std::vector
 using namespace std;
+
+class FileHandler {
+private:
+	fstream fileIn;
+	fstream fileOut;
+public:
+	FileHandler();
+	~FileHandler();
+	bool readLines(string fileName, vector<double>&);
+	void writeLines(string fileName, string writeStr);
+};
+
+class hexCharTable {
+private:
+	vector<char> table;
+public:
+	hexCharTable();
+	~hexCharTable();
+	char hexToChar(int);
+	int charToHex(char);
+};
+
+class degreeHexTable {
+private:
+	vector<int> table;
+	int hexInDegree;
+public:
+	degreeHexTable();
+	~degreeHexTable();
+	int degreeToHex(int);
+	int hexToDegree(int);
+};
+
+class CharClass {
+private:
+	char char1;
+	degreeHexTable angleTable;
+	hexCharTable hexTable;
+public:
+	CharClass(char);
+	~CharClass();
+	char toChar();
+	int degreeToHex;
+	void toHex(double&, double&);
+};
+
+class DegreePair {
+private:
+	int degree1;
+	int degree2;
+	const int maxDegree;
+	const int minDegree;
+	degreeHexTable angleTable;
+	hexCharTable hexTable;
+public:
+	DegreePair(double, double);
+	~DegreePair();
+	char toChar();
+};
+
+hexCharTable::hexCharTable() {
+	unsigned int i, n;
+	// build a lookup map
+	// hex values are actually represented in base10
+	for (i = 0, n = 128; i < n; i++) {
+		table[i] = char(i);
+	}
+}
+
+hexCharTable::~hexCharTable() {
+	// pointers were not used
+}
+
+char hexCharTable::hexToChar(int hex_) {
+	try {
+		return table[hex_];
+	} catch (...) {
+		return char(0);
+	}
+}
+
+int hexCharTable::charToHex(char char_) {
+	try {
+		return *(vector<char>::iterator) (find(table.begin(), table.end(),
+				char_));
+	} catch (...) {
+		return 0;
+	}
+}
+
+degreeHexTable::degreeHexTable() {
+	hexInDegree = 16;
+	unsigned int i, n;
+	// build a lookup map
+	for (i = 0, n = 360; i < n; i++) {
+		table[i] = (int) ((double) i / (360.0 / (double) hexInDegree));
+	}
+	table[360] = hexInDegree;
+}
+
+degreeHexTable::~degreeHexTable() {
+	// pointers were not used
+}
+
+int degreeHexTable::degreeToHex(int degree_) {
+	try {
+		return table[degree_];
+	} catch (...) {
+		return 0;
+	}
+}
+
+int degreeHexTable::hexToDegree(int hex_) {
+	try {
+		return *(vector<int>::iterator) (find(table.begin(), table.end(), hex_));
+	} catch (...) {
+		return 0;
+	}
+}
+
+FileHandler::FileHandler() {
+
+}
+FileHandler::~FileHandler() {
+
+}
+
+bool FileHandler::readLines(string filename, vector<double>& angles) {
+	ifstream in;    // Create an input file stream.
+	double angle;
+	in.open(filename);  // Use it to read from a file named data.txt.
+	cout << "whyyy";
+	if (in.is_open()) {
+		cout << "why";
+		while (in >> angle) {
+			cout << angle << endl;
+			angles.push_back(angle);
+		}
+		return true;
+	} else {
+		return false;
+	}
+}
+
+class Parser {
+private:
+	string degreeLine;
+	string charLine;
+	vector<DegreePair> degreePairList;
+	vector<CharClass> charClassList;
+public:
+	Parser(string);
+	~Parser();
+	vector<DegreePair> parseDegree(string);
+	vector<CharClass> parseChar(string);
+	string degreeLineToCharLine(string);
+	string charLineToDegreeLine(string);
+};
 
 class martian {
 public:
@@ -27,6 +189,22 @@ public:
 };
 
 int main() {
+	vector<double> angles;
+	FileHandler fh;
+	string fileName;
+	cout << "Please enter the file name and extension:" << endl;
+	cin >> fileName;
+	if(!fh.readLines(fileName, angles)){
+		cout << "Could not read the file." << endl;
+	} else {
+		cout << "File read." << endl;
+	}
+	unsigned int i, n;
+	n = angles.size();
+	for (i = 0; i < n; i++) {
+		cout << angles[i] << endl;
+	}
+
 	double x;
 	char ascii;
 	cin >> x;
@@ -35,5 +213,6 @@ int main() {
 		cout << "out of range";
 	else
 		cout << x << " hex:" << ascii;
+	cin >> x;
 	return 0;
 }
